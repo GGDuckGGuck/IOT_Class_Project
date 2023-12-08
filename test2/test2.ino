ef2794e335389ca623c6db45a87f46e800fbf4cc
GGDuckGGuck <rutc0512@naver.com>
2023-12-09 00:25:57 +0900

    Update test2.ino

--- test2/test2.ino
+++ test2/test2.ino
@@ -9,12 +9,10 @@ void setup() {
 }
 
 void scanBarcode() {
-  Serial.println("scanBarcode");
-
   while (barcode == "") {
-    Serial.println("Tlqkf");
-    while (GM65.available()) {
-      String barcode_Data = GM65.readStringUntil('\n');
+      while (GM65.available()) {
+      String barcode_Data = GM65.readStringUntil('
+');
       Serial.print("read : ");
       Serial.println(barcode_Data);
       barcode = barcode_Data;
@@ -24,7 +22,8 @@ void scanBarcode() {
 
 void loop() { 
   scanBarcode();
-  Serial.println("OUT");
+  char barcode_char = barcode.charAt(0); // String을 char로 변환합니다.
+  Serial.println(barcode_char); // char 형태의 barcode를 출력합니다.
   delay(2000);
 
   barcode = "";
